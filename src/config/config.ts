4792808f057edcc729a12d6f9336ccbaf334cf54
Võ  Phú <phuvo1104@gmail.com>
2026-05-25 13:02:31 +0700

    9

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import dotenv from 'dotenv'
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+dotenv.config({ path: path.resolve(__dirname, '../../.env'), override: true })
 
 const nodeEnv = process.env.NODE_ENV || 'development'
 
